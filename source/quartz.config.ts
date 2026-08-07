858734d1745a7463a2b848c07aa89fedebe02fde
lightbuck <bbb18.yt@gmail.com>
2026-08-07 15:20:27 +0200

    Hidden folders second attempt

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "tests", "test"],
+    ignorePatterns: ["private", "templates", ".obsidian", "**/tests", "**/test"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
